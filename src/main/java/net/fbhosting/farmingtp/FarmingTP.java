0b8a0b5872fc886e6bf5e4b4a1bb64b5296a26e7
Malte Linke <me@parzival.space>
2023-03-26 16:52:25 +0200

    added language support

--- src/main/java/net/fbhosting/farmingtp/FarmingTP.java
+++ src/main/java/net/fbhosting/farmingtp/FarmingTP.java
@@ -4,14 +4,20 @@ import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import net.fbhosting.farmingtp.commands.Farming;
 
 @Slf4j
 public final class FarmingTP extends JavaPlugin implements Listener {
+  @Getter
+  private static FarmingTP instance;
 
   @Override
   public void onEnable() {
+    instance = this; // NOSONAR - intended
+
+    // check dependencies
     if (Bukkit.getPluginManager().getPlugin("Multiverse-Core") == null) {
       log.error("Multiverse-Core not found! You are missing a dependency. Please install Multiverse-Core.");
       getServer().getPluginManager().disablePlugin(this);
@@ -23,7 +29,7 @@ public final class FarmingTP extends JavaPlugin implements Listener {
 
     // register commands
     getCommand("farming").setPermission("farmingtp.teleport");
-    getCommand("farming").setExecutor(new Farming(this));;
+    getCommand("farming").setExecutor(new Farming());;
   }
 
   @Override
